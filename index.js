448535a5491d0bc889b7927fb0d7814da899c37c
Lukas1h <Lukas1h7@gmail.com>
2023-04-18 09:17:57 -0700

    Uploaded wrong file.

--- index.js
+++ index.js
@@ -1,82 +1,178 @@
-import net from "net";
+import net from "net"
 import puppeteer from "puppeteer";
-
-import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config();
 // Define the port number that the server will listen on
-const PORT = 5000;
+
+const games = [
+  {
+    id: "3115a690-adc0-11ea-81fd-69ab84d66a7a",
+    name: "Zork",
+  },
+  {
+    id: "757929c1-6349-11ec-b65a-a9045c21f45d",
+    name: "Post Apocoliptic",
+  },
+  {
+    id: "b2c2cd71-e098-4f3d-9dd4-1d9759bc5fae",
+    name: "Highschool Life",
+  },
+];
+
+function searchGameByName(games, query) {
+  const pattern = new RegExp(query, "i"); // Create a regex pattern with the "i" flag for case-insensitivity
+  return games.find((game) => pattern.test(game.name));
+}
+
+
+const PORT = 5005;
 
 // Create a new TCP server
 const server = net.createServer();
+const browser = await puppeteer.launch({ headless: true });
 
 // When a client connects to the server
 server.on("connection", async (client) => {
-  console.log(
-    `Client connected from ${client.remoteAddress}:${client.remotePort}`
-  );
-
-  // Send a welcome message to the client
-  client.write(
-    "Welcome to AIDungeon. Use the `search` command to search for a game.\r\n\r\n"
-  );
-	client.write( ">");
-
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-
-	async function search(query){
-		console.log("searching ",query)
-		await page.goto(
-      "https://play.aidungeon.io/main/search?searchTerm="+encodeURIComponent(query),
-      { waitUntil: "networkidle0" }
+    console.log(
+        `Client connected from ${client.remoteAddress}:${client.remotePort}`
     );
-		const elements = await page.$$eval(
-      ".css-1dbjc4n.r-18u37iz>.css-901oao.css-nfaoni",
-      (elements) => {
-        return Array.from(elements, (element) => element.innerText);
-      }
-    );
-		
-		console.log(elements)
-		return elements
-	}
 
-	async function play(query){
-		console.log("searching ",query)
-		await page.goto(
-      "https://play.aidungeon.io/main/search?searchTerm="+encodeURIComponent(query),
-      { waitUntil: "networkidle0" }
-    );
-		var links = await page.$$(
-      ".css-1dbjc4n.r-1awozwy.r-18u37iz.r-16y2uox.r-u8s1d.r-zchlnj > *:last-child"
-    );
-		console.log(links)
-    await links[0].click();
-		
-	}
+    // Send a welcome message to the client
+    client.write(`\r\n\r\n            \u001b[7mWelcome To AIDungeon\u001b[0m\r\n`);
+    client.write(`
+Type \u001b[1mplay <game name>\u001b[0m to play a game.
+Type \u001b[1mlist\u001b[0m to list games.\r\n`);
+    client.write("\r\n\u001b[1m>");
 
-  
-  // When the client sends data to the server
-  client.on("data", async (data) => {
-    const command = data.toString().trim();
 
-    // Handle different commands here
-    if (command.startsWith("help")) {
-      client.write("Available commands: help, quit\r\n");
-    } else if (command.startsWith("search")) {
-      let results = await search(command.split(" ")[1]);
-			results.forEach((title)=>{
-				client.write(title+"\r\n");
-			})
-    } else if (command.startsWith("play")) {
-      await play(command.split(" ")[1]);
-    } else if (command.startsWith("quit")) {
-      client.write("Goodbye!\r\n");
-      client.end();
-    } else {
-      client.write(`Unknown command: ${command}\r\n`);
+
+    let text = ""
+    let lastTime = Date.now();
+    let isGen = false
+    let waitForNext = false;
+    let isPlaying = false;
+    const page = await browser.newPage();
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36')
+    async function play(name){
+        const game = searchGameByName(games, name);
+        
+        console.log(game)
+        if(game){
+            console.log("playing "+game.name)
+            console.log("id " + game.id);
+            isPlaying = true;
+            client.write("\r\n\u001b[1mLoading...");
+            await page.goto(
+              "https://play.aidungeon.io/main/scenarioPlay?publicId=" +
+                game.id,
+              { waitUntil: "networkidle0" }
+            );
+
+            if (game.id == "b2c2cd71-e098-4f3d-9dd4-1d9759bc5fae") {
+                console.log("highschool life")
+                await page.focus("textarea");
+                await page.keyboard.type("Lukas");
+                page.keyboard.press("Enter");
+                await page.focus("textarea");
+                await page.keyboard.type("Auburn");
+                page.keyboard.press("Enter");
+                await page.focus("textarea");
+                await page.keyboard.type("math");
+                page.keyboard.press("Enter");
+            }
+            if (game.id == "757929c1-6349-11ec-b65a-a9045c21f45d") {
+                console.log("post apo life")
+                await page.focus("textarea");
+                await page.keyboard.type("Lukas");
+                page.keyboard.press("Enter");
+            }
+
+
+            setInterval(async () => {
+            const innerTexts = await page.evaluate(() => {
+                const elements = document.querySelectorAll(
+                ".css-901oao.css-16my406.r-1xnzce8"
+                );
+                return Array.from(elements)
+                .map((element) => element.innerText)
+                .join();
+            });
+            if (text == innerTexts) {
+                //console.log("Same")
+                if (Date.now() - lastTime > 400 && isGen) {
+                isGen = false;
+                console.log("done generating");
+                doneText();
+                waitForNext = false;
+                }
+            } else {
+                //console.log("diffret")
+                if (true) {
+                if (isGen == false) {
+                    if (innerTexts.replaceAll(text, "").startsWith(",")) {
+                    console.log("STARTS WITH!");
+                    console.log(innerTexts.replaceAll(text, ""));
+                    }
+                    client.write("\u001b[0m\u001b[31m");
+                }
+                isGen = true;
+                if (!waitForNext) {
+                    newText(innerTexts.replaceAll(text, ""));
+                }
+                text = innerTexts;
+                lastTime = Date.now();
+                }
+            }
+            }, 20);
+        }else{
+            client.write("No games matched that name.");
+        }
+
+
+
+        
+
     }
-		client.write("\r\n >");
+
+    function newText(textNew){
+        client.write(textNew);
+    }
+    function doneText(){
+        if (!waitForNext) {
+            client.write("\u001b[0m\u001b[1m\r\n> ");
+        }
+    }
+
+  client.on("data", async (data) => {
+    if(isPlaying){
+        waitForNext = true;
+        await page.focus("textarea");
+        await page.keyboard.type(data.toString());
+        page.keyboard.press("Enter");
+        console.log(data.toString().includes("/say"));
+        console.log(data.toString());
+        if (data.toString().includes("/say")) {
+        console.log("include say");
+        waitForNext = false;
+        } else {
+        console.log("doesnt include say");
+        waitForNext = true;
+        }
+        console.log(waitForNext);
+    }else{
+        const command = data.toString().trim();
+        client.write("\u001b[0m");
+        if(command.startsWith("play")){
+            play(command.substring( command.indexOf(" ") + 1, command.length ))
+        }else if(command.startsWith("list")){
+            client.write("Games:\r\n");
+            games.forEach((game)=>{
+                client.write("\u001b[31m-" + game.name + "\u001b[0m\r\n");
+            })
+            
+        }
+        if(!isPlaying){
+            client.write("\r\n\u001b[1m>");
+        }
+    }
   });
 
   // When the client disconnects from the server
